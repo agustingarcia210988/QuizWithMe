99dcbd43b645e917992457dd6765de7a90e94526
Jason <lee.jason930@gmail.com>
2015-01-29 14:20:06 -0800

    added super basic base level exception catching so server doesnt crash at slightest breeze

--- app.js
+++ app.js
@@ -17,10 +17,10 @@
 	, QUESTION_TIMER_CYCLE = 30000;
 	
     
-//    app.use(function(err, req, res, next){
-//      console.error(err.stack);
-//      res.send(500, 'Our systems fell and are trying to get up sorry!');
-//    });
+    process.on('uncaughtException', function(ex) {
+        //lets hope it doesn't come to this
+        console.log(ex);
+    });
     
 	//use module.exports when exporting objects, arrays, groups of stuff.
 	//use exports when only exporting a single attr
@@ -241,7 +241,6 @@
 	 * assign api route connections
 	 */
 	app.get('/', function(req, res){
-        console.log(person.people);
 		//create a new default session if the current one doesn't exist
 		if(req.session.points === undefined){
 			console.log('session is brand new, give it session defaults');
